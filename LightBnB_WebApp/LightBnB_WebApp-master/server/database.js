0fb592d0fff851a2c873d30d00ed6ca4b664c1ce
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-10-15 03:22:16 +0000

    update getAllProperties in database

--- LightBnB_WebApp/LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp/LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,4 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
+const db = require('../db/index');
 
 /// Users
 
@@ -66,15 +65,16 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
-exports.getAllProperties = getAllProperties;
 
+const getAllProperties = function(options, limit = 10) {
+  const getAllProperties = function(options, limit = 10) {
+    return pool.query(`
+    SELECT * FROM properties
+    LIMIT $1
+    `, [limit])
+    .then(res => res.rows);
+  }
+}
 
 /**
  * Add a property to the database
