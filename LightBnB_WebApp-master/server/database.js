5b8492359e805529fbcb111639bdd4786fbb946a
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-10-15 16:14:21 +0000

    update functions to use the lightbnb database with SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,19 +17,26 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
-exports.getUserWithEmail = getUserWithEmail;
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE users.email = $1
+  `;
+  return pool.query(queryString, [email])
+    .then(res => {
+      if(res.rows) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+     })
+     .catch (err => {
+       console.log('query error:', err)
+     });
+};
+  exports.getUserWithEmail = getUserWithEmail;
 
 /**
  * Get a single user from the database given their id.
@@ -37,8 +44,21 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
-}
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE users.id = $1
+  `;
+  return pool.query(queryString, [id])
+    .then(res => {
+      if (res.rows) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch(err => console.log('query error:', err));
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -48,11 +68,21 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+  const values = [user.name, user.name, user.password];
+  return pool.query(queryString, values)
+    .then(res => {
+      return res.rows[0];
+    })
+    .catch(err => {
+      return console.log('query error:', err);
+    })
+};
+
 exports.addUser = addUser;
 
 /// Reservations
